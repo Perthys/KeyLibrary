cab096198b16f60dbebcc7448680ef75c027c2b3
Jason Jack Tan <52866402+Perthys@users.noreply.github.com>
2022-02-25 21:54:15 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -43,103 +43,190 @@ local EndedInputHandlers = {
 }
 
 function Key.new(...)
-    if type(select(2, ...)) ==  "string" then 
-        local Args = {...}
-        
-        local _Key = Args[1]
-        local Name = Args[2];  if not shared.KeyStorage[_Key] then shared.KeyStorage[_Key] = {}; end
-        local Handler = Args[3] 
-        local Type = Args[4] or "Tap"
-
-	    if shared.KeyStorage[_Key][Name] then
-	        if not shared.NameCount[Name] then shared.NameCount[Name] = 0 end
-	        
-	        shared.NameCount[Name] = shared.NameCount[Name] + 1 -- Some Parsers don't support +=
-		    Name = ("%s_%s"):format(Name, shared.NameCount[Name])
-	    end
-
-	    local self = {
-		    Key = _Key,
-		    Name = Name,
-		    Handler = Handler,
-		    Type = Type,
-		    LoopState = false;
-	    }
-
-	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
-
-	    shared.KeyStorage[_Key][Name] = Meta;
-
-	    return Meta;
-    elseif type(select(2, ...)) ==  "table" then 
-        local Args = {...}
-        
-        local _Key = Args[1]; if not shared.KeyStorage[_Key] then shared.KeyStorage[_Key] = {}; end
-        
-        local Name = Args[2]["Name"] or #shared.KeyStorage[_Key] + 1
-        local Handler = Args[2]["Handler"]
-        local Type = nil;
-        
-        if Args[2]["Type"] then
-            Type = Args[2]["Type"] 
-        else
-            Type = "Tap" 
+    if typeof(select(1, ...)) == "EnumItem" then
+        if type(select(2, ...)) ==  "string" then 
+            local Args = {...}
+            
+            local _Key = Args[1]
+            local Name = Args[2];  if not shared.KeyStorage[_Key] then shared.KeyStorage[_Key] = {}; end
+            local Handler = Args[3] 
+            local Type = Args[4] or "Tap"
+    
+    	    if shared.KeyStorage[_Key][Name] then
+    	        if not shared.NameCount[Name] then shared.NameCount[Name] = 0 end
+    	        
+    	        shared.NameCount[Name] = shared.NameCount[Name] + 1 -- Some Parsers don't support +=
+    		    Name = ("%s_%s"):format(Name, shared.NameCount[Name])
+    	    end
+    
+    	    local self = {
+    		    Key = _Key,
+    		    Name = Name,
+    		    Handler = Handler,
+    		    Type = Type,
+    		    LoopState = false;
+    	    }
+    
+    	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
+    
+    	    shared.KeyStorage[_Key][Name] = Meta;
+    
+    	    return Meta;
+        elseif type(select(2, ...)) ==  "table" then 
+            local Args = {...}
+            
+            local _Key = Args[1]; if not shared.KeyStorage[_Key] then shared.KeyStorage[_Key] = {}; end
+            
+            local Name = Args[2]["Name"] or #shared.KeyStorage[_Key] + 1
+            local Handler = Args[2]["Handler"]
+            local Type = nil;
+            
+            if Args[2]["Type"] then
+                Type = Args[2]["Type"] 
+            else
+                Type = "Tap" 
+            end
+    
+    	    if shared.KeyStorage[_Key][Name] then
+    	        if not shared.NameCount[Name] then shared.NameCount[Name] = 0 end
+    	        
+    	        shared.NameCount[Name] = shared.NameCount[Name] + 1 -- Some Parsers don't support +=
+    		    Name = ("%s_%s"):format(Name, shared.NameCount[Name])
+    	    end
+    
+    	    local self = {
+    		    Key = _Key,
+    		    Name = Name,
+    		    Handler = Handler,
+    		    Type = Type,
+    		    LoopState = false;
+    	    }
+    
+    	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
+    
+    	    shared.KeyStorage[_Key][Name] = Meta;
+    
+    	    return Meta;
+    	    
+    	elseif type(select(2, ...)) ==  "function" then 
+            local Args = {...}
+            
+            local _Key = Args[1];  if not shared.KeyStorage[_Key] then shared.KeyStorage[_Key] = {}; end
+            local Name = #shared.KeyStorage[_Key] + 1
+            local Handler = Args[2] 
+            local Type = Args[3] or "Tap"
+    
+    	    if shared.KeyStorage[_Key][Name] then
+    	        if not shared.NameCount[Name] then shared.NameCount[Name] = 0 end
+    	        
+    	        shared.NameCount[Name] = shared.NameCount[Name] + 1 -- Some Parsers don't support +=
+    		    Name = ("%s_%s"):format(Name, shared.NameCount[Name])
+    	    end
+    
+    	    local self = {
+    		    Key = _Key,
+    		    Name = Name,
+    		    Handler = Handler,
+    		    Type = Type,
+    		    LoopState = false;
+    	    }
+    
+    	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
+    
+    	    shared.KeyStorage[_Key][Name] = Meta;
+    
+    	    return Meta;
+    	end
+    elseif type(select(1, ...)) == "table" then
+        if type(select(2, ...)) == "string" then
+            local Args = {...}
+            
+            local Keys = Args[1]
+            local Name = Args[2];  
+            local Handler = Args[3] 
+            local Type = Args[4] or "Tap"
+            
+            local MultiArray = {}
+                
+            for Index, _Key in pairs(Keys) do
+                MultiArray[#MultiArray + 1] = Key.new(_Key, Name, Handler, Type);
+            end
+    
+    	    local self = {
+    		    Name = Name,
+    		    Handler = Handler,
+    		    Type = Type,
+    		    LoopState = false;
+    		    MultiArray = MultiArray
+    	    }
+    
+    	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
+    
+    	    return Meta;
+        elseif type(select(2, ...)) == "table" then
+            local Args = {...}
+            
+            local Keys = Args[1]
+            local Data = Args[2]
+            local Handler = Data["Handler"]
+            
+            local MultiArray = {}
+            
+            for Index, _Key in pairs(Keys) do
+                if Data["Name"] then
+                    MultiArray[#MultiArray + 1] = Key.new(_Key, Name, Handler, Type);
+                else
+                    MultiArray[#MultiArray + 1] = Key.new(_Key, Handler, Type);
+                end
+            end
+            
+            local self = {
+    		    Name = Name,
+    		    Handler = Handler,
+    		    Type = Type,
+    		    LoopState = false;
+    		    MultiArray = MultiArray
+    	    }
+    
+    	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
+    
+    	    return Meta;
+            
+        elseif type(select(2, ...)) == "function" then
+            local Args = {...}
+            
+            local Keys = Args[1]
+            local Handler = Args[2]
+            
+            local MultiArray = {}
+            
+            for Index, _Key in pairs(Keys) do
+                MultiArray[#MultiArray + 1] = Key.new(_Key, Handler, Type);
+            end
+            
+            local self = {
+    		    Name = Name,
+    		    Handler = Handler,
+    		    Type = Type,
+    		    LoopState = false;
+    		    MultiArray = MultiArray
+    	    }
+    
+    	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
+    
+    	    return Meta;
         end
-
-	    if shared.KeyStorage[_Key][Name] then
-	        if not shared.NameCount[Name] then shared.NameCount[Name] = 0 end
-	        
-	        shared.NameCount[Name] = shared.NameCount[Name] + 1 -- Some Parsers don't support +=
-		    Name = ("%s_%s"):format(Name, shared.NameCount[Name])
-	    end
-
-	    local self = {
-		    Key = _Key,
-		    Name = Name,
-		    Handler = Handler,
-		    Type = Type,
-		    LoopState = false;
-	    }
-
-	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
-
-	    shared.KeyStorage[_Key][Name] = Meta;
-
-	    return Meta;
-	    
-	elseif type(select(2, ...)) ==  "function" then 
-        local Args = {...}
-        
-        local _Key = Args[1];  if not shared.KeyStorage[_Key] then shared.KeyStorage[_Key] = {}; end
-        local Name = #shared.KeyStorage[_Key] + 1
-        local Handler = Args[2] 
-        local Type = Args[3] or "Tap"
-
-	    if shared.KeyStorage[_Key][Name] then
-	        if not shared.NameCount[Name] then shared.NameCount[Name] = 0 end
-	        
-	        shared.NameCount[Name] = shared.NameCount[Name] + 1 -- Some Parsers don't support +=
-		    Name = ("%s_%s"):format(Name, shared.NameCount[Name])
-	    end
-
-	    local self = {
-		    Key = _Key,
-		    Name = Name,
-		    Handler = Handler,
-		    Type = Type,
-		    LoopState = false;
-	    }
-
-	    local Meta = setmetatable(self, {__index = Key, __call = Handler});
-
-	    shared.KeyStorage[_Key][Name] = Meta;
-
-	    return Meta;
 	end
 end
 
 function Key.Unbind(self)
-	shared.KeyStorage[self.Key][self.Name] = nil;
+    if self.MultiArray then
+        for Index, Obj in pairs(self.MultiArray) do
+            Obj:Unbind()
+        end
+    else
+	    shared.KeyStorage[self.Key][self.Name] = nil;
+	end
 end
 
 if shared.InputBegan and shared.InputEnded then
@@ -185,4 +272,4 @@ UserInputService.InputEnded:Connect(function(Input, GameProcessedEvent)
 	end
 end)
 
-return Key, Key.new
+return Key, Key.new;
